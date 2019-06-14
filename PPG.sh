bbeaf968a67ac0abd8ccc3eb1cb118308653497f
pieterklap <47032688+pieterklap@users.noreply.github.com>
2019-06-14 14:37:57 +0200

    made the generation of the scripts skip-able

--- PPG.sh
+++ PPG.sh
@@ -95,6 +95,8 @@ while [ "$1" != "" ]; do
                             ;;
         -g | --genparam )   onlyparam="1"
                             ;;
+        -S | --noScripts )  Run_scripts="no"
+                            ;;
         -s | --shark )      SHARK="1"
                             shift                        # Allows Shark options to be entered
                             SHARKoptions=$1             # Allows Shark options to be entered
@@ -235,6 +237,9 @@ fi
 
 #   reapeat run parameters
 if [[ $RepeatRun_parameter_files == "yes" ]] && (($NUMparam>=2)); then
+    if [[ $Times_Run == "" ]]; then
+        Times_Run=0
+    fi
     for param_file in $paramsProg
     do
         #   Checks the header
@@ -247,7 +252,12 @@ if [[ $RepeatRun_parameter_files == "yes" ]] && (($NUMparam>=2)); then
         else
             sed_param_file=${param_file//\//\\/}
             sed_paramsProg=${paramsProg//\//\\/}
-            $PPG $(echo "$Entered_Command" | sed "s/$sed_paramsProg/$sed_param_file /")
+            if (($Times_Run==0)); then
+                $PPG $(echo "$Entered_Command" | sed "s/$sed_paramsProg/$sed_param_file /")
+            else
+                $PPG $(echo "$Entered_Command -S" | sed "s/$sed_paramsProg/$sed_param_file /")
+            fi
+            Times_Run=$[$Times_Run+1]
         fi
     done
     exit
@@ -383,243 +393,245 @@ if [[ $onlyparam == "1" ]] && [[ $LOC_Shared_param_file != "" ]]; then
 fi
 
 #   End of asigning/generating parameter files
+#   will skip generateing scripts if asked
+if [[ $Run_scripts != "no" ]]; then
 
-# Creates the files for the PIDs
-mkdir -vp "$LOC".PIDs
-rm -f "$LOC".PIDs/*
-# Creates the files that will use comet
-if [[ $Programs == *"comet"* ]]; then
-    touch "$LOC".PIDs/comet
-fi
-# Creates the files that will use Xtandem
-if [[ $Programs == *"tandem"* ]]; then
-    touch "$LOC".PIDs/Xtandem
-fi
-# Creates the files that will use MSGFPlus
-if [[ $Programs == *"msgfplus"* ]]; then
-    touch "$LOC".PIDs/MSGFPlus
-fi
-if [[ $Programs == *"msfragger"* ]]; then
-    touch "$LOC".PIDs/MSFragger
-fi
+#   Creates the files for the PIDs
+    mkdir -vp "$LOC".PIDs
+    rm -f "$LOC".PIDs/*
+#   Creates the files that will use comet
+    if [[ $Programs == *"comet"* ]]; then
+        touch "$LOC".PIDs/comet
+    fi
+#   Creates the files that will use Xtandem
+    if [[ $Programs == *"tandem"* ]]; then
+        touch "$LOC".PIDs/Xtandem
+    fi
+#   Creates the files that will use MSGFPlus
+    if [[ $Programs == *"msgfplus"* ]]; then
+        touch "$LOC".PIDs/MSGFPlus
+    fi
+    if [[ $Programs == *"msfragger"* ]]; then
+        touch "$LOC".PIDs/MSFragger
+    fi
 
-# done creating files for the PIDs
+#   done creating files for the PIDs
 
 
-# Adds the validator to the name of the scripts
-mkdir -vp "$LOC".VALs
-rm -f "$LOC".VALs/*
-# Creates the files that will use PeptideProphet or percolator
-if [[ $Programs == *"peptideprophet"* ]];then
-    for file in "$LOC".PIDs/*
+#   Adds the validator to the name of the scripts
+    mkdir -vp "$LOC".VALs
+    rm -f "$LOC".VALs/*
+#   Creates the files that will use PeptideProphet or percolator
+    if [[ $Programs == *"peptideprophet"* ]];then
+        for file in "$LOC".PIDs/*
+        do
+            cp ${file} ${file}_PeptideProphet
+            cp "$LOC".PIDs/*_PeptideProphet "$LOC".VALs/
+            rm -f "$LOC".PIDs/*_PeptideProphet
+        done
+    fi
+    if [[ $Programs == *"percolator"* ]];then
+        for file in "$LOC".PIDs/*
+        do
+            cp ${file} ${file}_percolator
+            cp "$LOC".PIDs/*_percolator "$LOC".VALs/
+            rm -f "$LOC".PIDs/*_percolator
+        done
+    fi
+
+#   checks if a validator has been used if not it copies everyting in the .PIDs directory to the .VALs directory
+#   It also sets NOVAL to 1 to stop analysis programs from being added
+    VALcheck=$(ls "$LOC".VALs)
+    if [[ $VALcheck == "" ]]; then
+        cp "$LOC".PIDs/* "$LOC".VALs/
+        NOVAL="1"
+    fi
+#   done adding validators to the name of the scripts
+
+#   Counts the amount of scripts that have been generated
+    NUM=$(ls "$LOC".VALs/ | wc -l)
+
+#   adds .sh to the files
+    LOC=$(echo "$0" |awk -F/ '{$NF="";print $0}' | tr " " "/")
+
+    mkdir -vp "$LOC".END
+    rm -f "$LOC".END/*
+    for file in "$LOC".VALs/*
     do
-        cp ${file} ${file}_PeptideProphet
-        cp "$LOC".PIDs/*_PeptideProphet "$LOC".VALs/
-        rm -f "$LOC".PIDs/*_PeptideProphet
+        cp ${file} ${file}.sh
+        cp "$LOC".VALs/*.sh "$LOC".END/
+        rm -f "$LOC".VALs/*.sh
     done
-fi
-if [[ $Programs == *"percolator"* ]];then
-    for file in "$LOC".PIDs/*
-    do
-        cp ${file} ${file}_percolator
-        cp "$LOC".PIDs/*_percolator "$LOC".VALs/
-        rm -f "$LOC".PIDs/*_percolator
-    done
-fi
 
-# checks if a validator has been used if not it copies everyting in the .PIDs directory to the .VALs directory
-# It also sets NOVAL to 1 to stop analysis programs from being added
-VALcheck=$(ls "$LOC".VALs)
-if [[ $VALcheck == "" ]]; then
-    cp "$LOC".PIDs/* "$LOC".VALs/
-    NOVAL="1"
-fi
-# done adding validators to the name of the scripts
-
-# Counts the amount of scripts that have been generated
-NUM=$(ls "$LOC".VALs/ | wc -l)
-
-# adds .sh to the files
-LOC=$(echo "$0" |awk -F/ '{$NF="";print $0}' | tr " " "/")
-
-mkdir -vp "$LOC".END
-rm -f "$LOC".END/*
-for file in "$LOC".VALs/*
-do
-    cp ${file} ${file}.sh
-    cp "$LOC".VALs/*.sh "$LOC".END/
-    rm -f "$LOC".VALs/*.sh
-done
-
-# Creates the directory scripts and copies the scripts to it and makes them executable and removes the files in the temp folders
-if [ ! -d "$LOCscripts" ]; then
-    mkdir -vp "$LOCscripts"
-else
-    rm  "$LOCscripts"*
-fi
-cp "$LOC".END/* "$LOCscripts"
-chmod 750 "$LOCscripts"*
-rm -f "$LOC".PIDs/* "$LOC".VALs/*
-rm -f "$LOC".END/*
-
-# TODO a variable that is "$LOC/src/base_scripts/"
-
-# Adds the options fille to all the scripts
-# this should always be first
-for file in "$LOCscripts"*.sh
-do
-    cat "$LOC"src/base_scripts/options > ${file}
-    #    changes PIDhelp_to_be_replaced and VALhelp_to_be_replaced to the name of the PID/VAL
-    PIDHELP=$(echo ${file} | awk -F\/ '{print $NF}' | awk -F_ '{print $1}')
-    VALHELP=$(echo ${file} | awk -F_ '{print $2}' | awk -F\. '{print $1}')
-    LOC_sed=${LOC//\//\\/}
-    sed -i "s/LOChelp_to_be_replaced/$LOC_sed/" ${file}
-    sed -i "s/PIDhelp_to_be_replaced/$PIDHELP/" ${file}
-    sed -i "s/VALhelp_to_be_replaced/$VALHELP/" ${file}
-done
-
-# Makes changes to the parameter files
-# Changes the comet output file from pep.xml to PIN
-for file in "$LOCscripts"*comet_percolator*
-do
-    cat "$LOC"src/base_scripts/comet2pin >> ${file}
-done
-for file in "$LOCscripts"*MSGFPlus_percolator*
-do
-    cat "$LOC"src/base_scripts/MSGF2percolator >> ${file}
-done
+#   Creates the directory scripts and copies the scripts to it and makes them executable and removes the files in the temp folders
+    if [ ! -d "$LOCscripts" ]; then
+        mkdir -vp "$LOCscripts"
+    else
+        rm  "$LOCscripts"*
+    fi
+    cp "$LOC".END/* "$LOCscripts"
+    chmod 750 "$LOCscripts"*
+    rm -f "$LOC".PIDs/* "$LOC".VALs/*
+    rm -f "$LOC".END/*
 
 
-
-# starts adding PIDs to the scripts
-# Adds the comet file to all the scripts containing comet
-for file in "$LOCscripts"*comet*
-do
-    cat "$LOC"src/base_scripts/comet >> ${file}
-done
-# Adds the Xtandem file to all the scripts containing Xtandem
-for file in "$LOCscripts"*Xtandem*
-do
-    cat "$LOC"src/base_scripts/Xtandem >> ${file}
-done
-# adds the MSGFPlus file to all the scripts containing MSGFPlus
-for file in "$LOCscripts"*MSGFPlus*
-do
-    cat "$LOC"src/base_scripts/MSGFPlus >> ${file}
-done
-for file in "$LOCscripts"*MSFragger*
-do
-    cat "$LOC"src/base_scripts/MSFragger >> ${file}
-done
-
-# done with adding PIDs to the scripts
-
-# Add an intermediate time to the script
-for file in "$LOCscripts"*.sh
-do
-    cat "$LOC"src/base_scripts/"time" >> ${file}
-done
-
-
-# starts adding converters to the scripts
-# Adds the Tandem2XML file to all the scripts that contain Xtandem and Peptideprophet
-for file in "$LOCscripts"*Xtandem_PeptideProphet*
-do
-    cat "$LOC"src/base_scripts/Tandem2XML >> ${file}
-done
-for file in "$LOCscripts"*MSGFPlus_PeptideProphet*
-do
-    cat "$LOC"src/base_scripts/idconvert >> ${file}
-done
-for file in "$LOCscripts"*Xtandem_percolator*
-do
-    cat "$LOC"src/base_scripts/tandem2pin >> ${file}
-done
-for file in "$LOCscripts"*MSGFPlus_percolator*
-do
-    cat "$LOC"src/base_scripts/msgf2pin >> ${file}
-done
-for file in "$LOCscripts"*MSFragger_percolator*
-do
-    cat "$LOC""src/base_scripts/make-pin" >> ${file}
-done
-
-#done adding converters to the scripts
-
-# starts adding validators to the scripts
-# Adds the PeptideProphet file to all the scripts containing peptideprophet
-for file in "$LOCscripts"*PeptideProphet*
-do
-    cat "$LOC"src/base_scripts/PeptideProphet >> ${file}
-done
-# Adds the percolator file to all the scripts containing percolator
-for file in "$LOCscripts"*percolator*
-do
-    cat "$LOC"src/base_scripts/percolator >> ${file}
-done
-# done adding validators to the scripts
-
-# Add an intermediate time to the script
-for file in "$LOCscripts"*.sh
-do
-    cat "$LOC"src/base_scripts/"time" >> ${file}
-done
-
-# Adds analysis tools to the pipeline
-if [[ $Programs == *"gprofiler"* ]] && [[ $NOVAL != "1" ]]; then
+#   Adds the options fille to all the scripts
+#   this should always be first
     for file in "$LOCscripts"*.sh
     do
-        cat "$LOC"src/base_scripts/gprofiler >> ${file}
+        cat "$LOC"src/base_scripts/options > ${file}
+    #   changes PIDhelp_to_be_replaced and VALhelp_to_be_replaced to the name of the PID/VAL
+        PIDHELP=$(echo ${file} | awk -F\/ '{print $NF}' | awk -F_ '{print $1}')
+        VALHELP=$(echo ${file} | awk -F_ '{print $2}' | awk -F\. '{print $1}')
+        LOC_sed=${LOC//\//\\/}
+        sed -i "s/LOChelp_to_be_replaced/$LOC_sed/" ${file}
+        sed -i "s/PIDhelp_to_be_replaced/$PIDHELP/" ${file}
+        sed -i "s/VALhelp_to_be_replaced/$VALHELP/" ${file}
     done
-fi
-if [[ $Programs == *"reactome"* ]] && [[ $NOVAL != "1" ]]; then
-    for file in "$LOCscripts"/*.sh
+
+#   Makes changes to the parameter files
+#   Changes the comet output file from pep.xml to PIN
+    for file in "$LOCscripts"*comet_percolator*
     do
-        cat "$LOC"src/base_scripts/Reactome >> ${file}
+        cat "$LOC"src/base_scripts/comet2pin >> ${file}
     done
-fi
+    for file in "$LOCscripts"*MSGFPlus_percolator*
+    do
+        cat "$LOC"src/base_scripts/MSGF2percolator >> ${file}
+    done
+
+
+
+#   starts adding PIDs to the scripts
+#   Adds the comet file to all the scripts containing comet
+    for file in "$LOCscripts"*comet*
+    do
+        cat "$LOC"src/base_scripts/comet >> ${file}
+    done
+#   Adds the Xtandem file to all the scripts containing Xtandem
+    for file in "$LOCscripts"*Xtandem*
+    do
+        cat "$LOC"src/base_scripts/Xtandem >> ${file}
+    done
+#   adds the MSGFPlus file to all the scripts containing MSGFPlus
+    for file in "$LOCscripts"*MSGFPlus*
+    do
+        cat "$LOC"src/base_scripts/MSGFPlus >> ${file}
+    done
+    for file in "$LOCscripts"*MSFragger*
+    do
+        cat "$LOC"src/base_scripts/MSFragger >> ${file}
+    done
+
+#   done with adding PIDs to the scripts
+
+#   Add an intermediate time to the script
+    for file in "$LOCscripts"*.sh
+    do
+        cat "$LOC"src/base_scripts/"time" >> ${file}
+    done
+
+
+#   starts adding converters to the scripts
+#   Adds the Tandem2XML file to all the scripts that contain Xtandem and Peptideprophet
+    for file in "$LOCscripts"*Xtandem_PeptideProphet*
+    do
+        cat "$LOC"src/base_scripts/Tandem2XML >> ${file}
+    done
+    for file in "$LOCscripts"*MSGFPlus_PeptideProphet*
+    do
+        cat "$LOC"src/base_scripts/idconvert >> ${file}
+    done
+    for file in "$LOCscripts"*Xtandem_percolator*
+    do
+        cat "$LOC"src/base_scripts/tandem2pin >> ${file}
+    done
+    for file in "$LOCscripts"*MSGFPlus_percolator*
+    do
+        cat "$LOC"src/base_scripts/msgf2pin >> ${file}
+    done
+    for file in "$LOCscripts"*MSFragger_percolator*
+    do
+        cat "$LOC""src/base_scripts/make-pin" >> ${file}
+    done
+
+#   done adding converters to the scripts
+
+#   starts adding validators to the scripts
+#   Adds the PeptideProphet file to all the scripts containing peptideprophet
+    for file in "$LOCscripts"*PeptideProphet*
+    do
+        cat "$LOC"src/base_scripts/PeptideProphet >> ${file}
+    done
+#   Adds the percolator file to all the scripts containing percolator
+    for file in "$LOCscripts"*percolator*
+    do
+        cat "$LOC"src/base_scripts/percolator >> ${file}
+    done
+#   done adding validators to the scripts
+
+#   Add an intermediate time to the script
+    for file in "$LOCscripts"*.sh
+    do
+        cat "$LOC"src/base_scripts/"time" >> ${file}
+    done
+
+#   Adds analysis tools to the pipeline
+    if [[ $Programs == *"gprofiler"* ]] && [[ $NOVAL != "1" ]]; then
+        for file in "$LOCscripts"*.sh
+        do
+            cat "$LOC"src/base_scripts/gprofiler >> ${file}
+        done
+    fi
+    if [[ $Programs == *"reactome"* ]] && [[ $NOVAL != "1" ]]; then
+        for file in "$LOCscripts"/*.sh
+        do
+            cat "$LOC"src/base_scripts/Reactome >> ${file}
+        done
+    fi
 
 #   add the ending part to the script needed to know how long it took to run the scripts
-for file in "$LOCscripts"*.sh
-do
-    cat "$LOC"src/base_scripts/End >> ${file}
-    echo "Generated ${file}"
-done
+    for file in "$LOCscripts"*.sh
+    do
+        cat "$LOC"src/base_scripts/End >> ${file}
+        echo "Generated ${file}"
+    done
 
-# puts the name of the script in the script (changes the name of the job on the shark cluster)
-for file in "$LOCscripts"*.sh
-do
-    Script_Name=$(echo ${file} | awk -F\/ '{print $NF}' | awk -F. '{print $1}')
-    sed -i "s/Script_Name_shark_cluster/$Script_Name/" ${file}
-done
-# The pipeline generates a file named *[program]* if the program was not selected
-# the following code removes the files that were created when a program was not selected
-# All new programs in the pipeline should be added above this line of code
-for file in "$LOCscripts"\**
-do
-    rm -f "${file}"
-done
+#   puts the name of the script in the script (changes the name of the job on the shark cluster)
+    for file in "$LOCscripts"*.sh
+    do
+        Script_Name=$(echo ${file} | awk -F\/ '{print $NF}' | awk -F. '{print $1}')
+        sed -i "s/Script_Name_shark_cluster/$Script_Name/" ${file}
+    done
+
+#   The pipeline generates a file named *[program]* if the program was not selected
+#   the following code removes the files that were created when a program was not selected
+#   All new programs in the pipeline should be added above this line of code
+    for file in "$LOCscripts"\**
+    do
+        rm -f "${file}"
+    done
 
 #   copies the install_locations to the locations of the scripts
-cp -v "$LOC"install_locations "$LOCscripts"
+    cp -v "$LOC"install_locations "$LOCscripts"
 
-# tells the user the scripts are generated
-if (($NUM == 1)); then
-    echo "$NUM script has been generated"
-else
-    echo "$NUM scripts have been generated"
-fi
+#   tells the user the scripts are generated
+    if (($NUM == 1)); then
+        echo "$NUM script has been generated"
+    else
+        echo "$NUM scripts have been generated"
+    fi
 
 #   Tells the user if a program has not been added because a previous program was required
-if [[ $NOVAL == "1" ]]; then
-    if [[ $Programs == *"gprofiler"* ]]; then
-        echo -e "\e[93mWARNING:\e[0m g:profiler isn't added to the script because it requires at least one validator"
-    fi
-    if [[ $Programs == *"reactome"* ]]; then
-        echo -e "\e[93mWARNING:\e[0m Reactome isn't added to the script because it requires at least one validator"
+    if  [[ $NOVAL == "1" ]]; then
+        if [[ $Programs == *"gprofiler"* ]]; then
+            echo -e "\e[93mWARNING:\e[0m g:profiler isn't added to the script because it requires at least one validator"
+        fi
+        if [[ $Programs == *"reactome"* ]]; then
+            echo -e "\e[93mWARNING:\e[0m Reactome isn't added to the script because it requires at least one validator"
+        fi
     fi
 fi
-
 #   Load the file that runs the scripts
 source $LOC/src/run_pipeline.sh
 
